5494e9091222593840d0a46e6486e6948bd2a8bf
Muhammed Sayar <muhammedsayar47@gmail.com>
2023-05-26 01:32:08 +0300

    V2.2 Update
    Add login system but only Index login
    remember import SIS.Data db

--- SIS/Controllers/LoginController.cs
+++ SIS/Controllers/LoginController.cs
@@ -1,21 +1,53 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SIS.Data;
+using SIS.Models;
+using System.Data;
 
 namespace SIS.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: LoginController
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    Login model = new Login();
+        //    return View(model);
+        //}
+
+        public IActionResult Index(Login model)
         {
-            return View();
+            using (var _connection = new SqlConnection("Data Source=.;Initial Catalog=SIS.Data;Integrated Security=True"))
+            {
+                _connection.Open();
+
+                if (ModelState.IsValid)
+                {
+                    // Authenticate the user
+                    var query = @"SELECT * FROM AuthorizedUser WHERE Username = @Username AND Password = @Password";
+
+                    using (var cmd = _connection.CreateCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Parameters.AddWithValue("@Username", model.Username);
+                        cmd.Parameters.AddWithValue("@Password", model.Password);
+
+                        var reader = cmd.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            // Redirect the user to the home page
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                }
+
+                // Show an error message
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
         }
 
-        // GET: LoginController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+    }
 
 
         public ActionResult LoginAdmin()
--- SIS/Models/Login.cs
+++ SIS/Models/Login.cs
@@ -1,9 +1,17 @@
-﻿namespace SIS.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIS.Models
 {
     public class Login
     {
         public int Id { get; set; }
         public string? StudentNo { get; set; }
         public string? IdentificationNumber { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+
+        [NotMapped]
+        public string Password { get; set; }
+
     }
 }
